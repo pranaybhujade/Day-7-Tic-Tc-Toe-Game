59a80bc708064d679320371f39c0163ab586c3d4
pranaybhujade <pranaybhujade@gmail.com>
2022-09-03 10:08:48 +0530

    toss to check who plays first computer or player

--- Tic Tac Toe Game/src/com/bridgelabz/TicTacToeGame.java
+++ Tic Tac Toe Game/src/com/bridgelabz/TicTacToeGame.java
@@ -2,20 +2,27 @@ package com.bridgelabz;
 
 
 public class TicTacToeGame {
-    static char player2letter = ' ';
-    static Scanner scanner = new Scanner(System.in);
-    static char[] board = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',};
-
     public static void main(String[] args) {
+        TicTacToeGame gameObj = new TicTacToeGame();
+        gameObj.initialiseGame();
+        if (gameObj.toss() == 1)
+            System.out.println("\nPlayer won the toss.\nPlayer plays first.");
+        else
+            System.out.println("\nComputer won the toss.\nComputer plays first.");
+        gameObj.choosePlayerLetter();
+        gameObj.showBoard();
+        gameObj.playerPlays();
+         else if (gameBoard[playerCell] != ' ') {
+		else
+            gameBoard[playerCell] = playerLetter;
+        }
 
-
-        public static void main (String[]args){
-            System.out.println("Welcome to Tic Tac Toe Game");
-
-            player1letter = chooseLetter();
+        int toss() {
+            return (Math.random() > 0.5 ? 1 : 0);
+    }
         }
     }
-}
+
 
 
 
