b4a6899b73a5cef3c2cce1659d6686ea303f1902
pranaybhujade <pranaybhujade@gmail.com>
2022-09-03 09:48:48 +0530

    Display current board so that player can choose valid cells

--- Tic Tac Toe Game/src/com/bridgelabz/TicTacToeGame.java
+++ Tic Tac Toe Game/src/com/bridgelabz/TicTacToeGame.java
@@ -1,44 +1,33 @@
 package com.bridgelabz;
 
-import java.util.Scanner;
 
 public class TicTacToeGame {
-    char gameBoard[] = null;
-    char playerLetter = '\0';
-    char computerLetter = '\0';
-
     public static void main(String[] args) {
-        System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
-        System.out.println("In this program you will compete to win in a Tic Tac Toe game against the Computer.\n");
-
         TicTacToeGame gameObj = new TicTacToeGame();
         gameObj.initialiseGame();
         gameObj.choosePlayerLetter();
+        gameObj.showBoard();
     }
 
     void initialiseGame(){
-        gameBoard = new char[10];
-        for(int index = 0; index < 10; index++)
-            gameBoard[index] = ' ';
+         else if (playerInput == 'O' || playerInput == 'o') {
+		else
+            {
+            System.out.println("\nInvalid Input.\nPlease try again!");
+        }
+
+        void showBoard() {
+            System.out.println("\nCurrent Board : ");
+            System.out.println("-------------");
+            System.out.println("| "+gameBoard[1]+" | "+gameBoard[2]+" | "+gameBoard[3]+" |");
+            System.out.println("-------------");
+            System.out.println("| "+gameBoard[4]+" | "+gameBoard[5]+" | "+gameBoard[6]+" |");
+            System.out.println("-------------");
+            System.out.println("| "+gameBoard[7]+" | "+gameBoard[8]+" | "+gameBoard[9]+" |");
+            System.out.println("-------------");
     }
 
-    void choosePlayerLetter() {
-        System.out.println("\nPlayer please choose your play Letter.");
-        System.out.println("Enter 'X' to play 'X' on your turn.");
-        System.out.println("Or Enter 'O' to play 'O' on your turn.");
-        Scanner sc = new Scanner(System.in);
-        char playerInput = sc.next().charAt(0);
-        sc.close();
-        if (playerInput == 'X' || playerInput == 'x') {
-            playerLetter = 'X';
-            computerLetter = 'O';
-        }
-        else if (playerInput == 'O' || playerInput == 'o') {
-            playerLetter = 'O';
-            computerLetter = 'o';
-        }
-        else
-            System.out.println("\nInvalid Input.\nPlease try again!");
-    }
-    }
+}
+
+
 
