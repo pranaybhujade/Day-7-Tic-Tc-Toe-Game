36657bf0fd67b6b54bfd6f0fe31f22252c4ca71c
pranaybhujade <pranaybhujade@gmail.com>
2022-09-03 09:55:39 +0530

    Player makes move to desired location

--- Tic Tac Toe Game/src/com/bridgelabz/TicTacToeGame.java
+++ Tic Tac Toe Game/src/com/bridgelabz/TicTacToeGame.java
@@ -2,32 +2,54 @@ package com.bridgelabz;
 
 
 public class TicTacToeGame {
-    public static void main(String[] args) {
-        TicTacToeGame gameObj = new TicTacToeGame();
-        gameObj.initialiseGame();
-        gameObj.choosePlayerLetter();
-        gameObj.showBoard();
-    }
+    char gameBoard[] = null;
+    char playerLetter = '\0';
+    char computerLetter = '\0';
+    static Scanner sc = new Scanner(System.in);
 
-    void initialiseGame(){
-         else if (playerInput == 'O' || playerInput == 'o') {
-		else
-            {
-            System.out.println("\nInvalid Input.\nPlease try again!");
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Tic Tac Toe Game Program in Java!");
+        public static void main (String[]args){
+            gameObj.initialiseGame();
+            gameObj.choosePlayerLetter();
+            gameObj.showBoard();
+            gameObj.playerPlays();
         }
 
-        void showBoard() {
-            System.out.println("\nCurrent Board : ");
-            System.out.println("-------------");
-            System.out.println("| "+gameBoard[1]+" | "+gameBoard[2]+" | "+gameBoard[3]+" |");
-            System.out.println("-------------");
-            System.out.println("| "+gameBoard[4]+" | "+gameBoard[5]+" | "+gameBoard[6]+" |");
-            System.out.println("-------------");
-            System.out.println("| "+gameBoard[7]+" | "+gameBoard[8]+" | "+gameBoard[9]+" |");
-            System.out.println("-------------");
-    }
+        void initialiseGame () {
+            void choosePlayerLetter () {
+                System.out.println("\nPlayer please choose your play Letter.");
+                System.out.println("Enter 'X' to play 'X' on your turn.");
+                System.out.println("Or Enter 'O' to play 'O' on your turn.");
+                Scanner sc = new Scanner(System.in);
+                char playerInput = sc.next().charAt(0);
+                sc.close();
+                if (playerInput == 'X' || playerInput == 'x') {
+                    playerLetter = 'X';
+                    computerLetter = 'O';
+                    void showBoard () {
+                        System.out.println("| " + gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9] + " |");
+                        System.out.println("-------------");
+                    }
 
-}
+                    void playerPlays () {
+                        System.out.print("\nEnter an empty cell number [1-9] where do want make your move : ");
+                        byte playerCell = sc.nextByte();
+                        if (playerCell > 9 || playerCell < 1) {
+                            System.out.println("\nInvalid selection.\nPlease try again!");
+                            playerPlays();
+                        } else if (gameBoard[playerCell] != ' ') {
+                            System.out.println("\nThe cell you selected is not empty.\nPlease try again!");
+                            playerPlays();
+                        } else
+                            gameBoard[playerCell] = playerLetter;
+                    }
+
+                }
+            }
+        }
+    }
 
 
 
